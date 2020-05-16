76e56e77f812d91bc3ff4d7cb00643ea5a6786a0
kennamz <kenna@zimfam.org>
2020-05-16 11:47:24 -0600

    added a fish egg sprite that disappears when you click it

---
+++ Kenna's First Unity Game/Assets/Eggs.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eggs : MonoBehaviour
+{
+    private Transform _myTransform;
+    private GameObject eggSprite;
+    private Transform _eggSpriteTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myTransform = this.transform;
+        _myTransform.gameObject.SetActive(true);
+
+        eggSprite = GameObject.Find("Eggs");
+        _eggSpriteTransform = eggSprite.transform;
+        _eggSpriteTransform.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    // Called when the collider is clicked
+    void OnMouseDown()
+    {
+        _myTransform.gameObject.SetActive(false);
+        _eggSpriteTransform.gameObject.SetActive(false);
+    }
+}
