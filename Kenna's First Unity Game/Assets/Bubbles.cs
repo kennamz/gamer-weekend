83a1cd39860a6a1292dd761081c3af6a3d63ebdb
kennamz <kenna@zimfam.org>
2020-05-17 16:55:36 -0600

    added a treasure chest which occasionally opens and releases an air bubble

---
+++ Kenna's First Unity Game/Assets/Bubbles.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubbles : MonoBehaviour
+{
+    public float Speed = 5f;
+    private Transform _myTransform;
+    Vector3 StartingPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myTransform = this.transform;
+        StartingPosition = new Vector3(-7, -2.289078f, -1);
+        _myTransform.position = StartingPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.activeSelf)
+        {
+            var moveAmount = Speed * Time.deltaTime;
+            _myTransform.position += new Vector3(0, moveAmount, 0);
+            if (_myTransform.position.y >= 13)
+            {
+                _myTransform.gameObject.SetActive(false);
+                _myTransform.position = StartingPosition;
+            }
+        }
+    }
+}
---
+++ Kenna's First Unity Game/Assets/TreasureChest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChest : MonoBehaviour
+{
+    private Transform _myTransform;
+
+    private int count = 0;
+    public int Timer = 160; //determines how often the bubble will be released
+    public int openTime = 120;
+    public Sprite chestOpen;
+    public Sprite chestClosed;
+
+    private GameObject bubbles;
+    private Transform _bubblesTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myTransform = this.transform;
+
+        bubbles = GameObject.Find("bubbles");
+        _bubblesTransform = bubbles.transform;
+        _bubblesTransform.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count++;
+        if (count >= Timer)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = chestOpen;
+            count = -1 * openTime;
+            _bubblesTransform.gameObject.SetActive(true);
+        }
+        else if (count < 0)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = chestOpen;
+        }
+        else
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = chestClosed;
+        }
+    }
+}
