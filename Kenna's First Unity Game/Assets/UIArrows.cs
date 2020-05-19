7cd64b0acb51e79b98488b1fc8e5982474d1bdb7
kennamz <kenna@zimfam.org>
2020-05-19 12:05:07 -0600

    added UI elements for tutorial and fish food can, added fish flakes sprite

---
+++ Kenna's First Unity Game/Assets/UIArrows.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIArrows : MonoBehaviour
+{
+    private Transform _myTransform;
+
+    private int count = 0;
+    public int Timer = 160;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myTransform = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count++;
+        if (count >= Timer)
+        {
+            _myTransform.gameObject.SetActive(false);
+        }
+    }
+}
